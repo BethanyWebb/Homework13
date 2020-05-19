e9148962be2e8ae7432974b556699bf75bafe6e3
Bywebb306 <bywebb306@gmail.com>
2020-05-18 22:16:33 -0400

    uploading homework13

---
+++ HWSTARTER/Burger/public/assets/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $(".devour-form").on("submit", function(event) {
+    event.preventDefault();
+    var burger_id = $(this).children(".burger_id").val();
+    console.log(burger_id);
+    // Send the PUT request.
+    $.ajax({
+      method: "PUT",
+      url: "/burgers/" + burger_id
+    }).then(
+      function(data) {
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
---
+++ HWSTARTER/README.md
@@ -0,0 +1,2 @@
+# Homework13
+Burger App using node.js, express, MySql
